6d41b3a70d62c568d3168cdcb296d05c08165879
bbakjun <wnsguddl789@gmail.com>
2022-07-23 19:16:36 +0900

    Jay : 헤더 생성 및 로그인페이지

---
+++ src/components/Layout.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components';
+import Header from './Header';
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FunctionComponent<LayoutProps> = ({children}) => {
+  return (
+    <S.Container>
+      <Header></Header>
+      <S.Main>{children}</S.Main>
+    </S.Container>
+  );
+};
+
+export {Layout};
+
+const S = {
+  Container: styled.div`
+    display: flex;
+    flex: 1;
+    height: 100%;
+    flex-direction: column;
+    align-items: center;
+  `,
+  Main: styled.main`
+    background-color: #f5f7fa;
+    width: 100%;
+    display: flex;
+    flex: 1;
+    min-height: calc(100vh - 88px);
+    height: 100%;
+  `,
+};
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,27 +1,51 @@
-import React from "react";
-import styled, { DefaultTheme, useTheme } from "styled-components";
-import Typography from "../components/atoms/typography";
+import React from 'react';
+import styled, {DefaultTheme, useTheme} from 'styled-components';
+import axios from 'axios';
+import {GithubIcon} from '../components/Icon';
+import Typography from '../components/atoms/typography';
+
+const GITHUB_END_POINT = `https://github.com/login/oauth/authorize?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&scope=repo:status read:repo_hook user:email&redirect_uri=http://localhost:3000/callback`;
+
+type OauthParameterType = {
+  clientId: string | undefined;
+};
 
 const Login = () => {
-  const theme = useTheme();
   return (
     <S.Container>
-      <Typography size="13" marginBottom={10}>
-        안녕하세요
+      <Typography weight={'700'} color="#212121" marginBottom={24}>
+        모두의 레포지토리 MO:RE
       </Typography>
-      {/* <TypographyParent size="12"></TypographyParent> */}
+      <Typography weight={'500'} color={'##4D4D4D'} marginBottom={48}>
+        Github 계정으로 자랑하고 싶은 내 레포지토리를 올려 볼까요?
+      </Typography>
+      <S.LoginButton onClick={() => window.location.replace(GITHUB_END_POINT)}>
+        <GithubIcon />
+        <Typography size="13">Github 계정으로 로그인</Typography>
+      </S.LoginButton>
     </S.Container>
   );
 };
 
 export default Login;
 
-const Container = styled.div``;
-
-// const TypographyParent = styled(Typography)`
-//   background-color: red;
-// `;
-
 const S = {
-  Container: styled.div``,
+  Container: styled.div`
+    display: flex;
+    flex: 1;
+    height: 100%;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  `,
+  LoginButton: styled.button`
+    display: flex;
+    height: 100%;
+    align-items: center;
+    display: flex;
+    padding: 20px 90px;
+    gap: 10px;
+    border: 1px solid #abbed1;
+    border-radius: 8px;
+  `,
 };
