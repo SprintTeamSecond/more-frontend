ac82e7196c0f25937cb72b8a14a33663dbf95f56
bbakjun <wnsguddl789@gmail.com>
2022-07-24 22:43:17 +0900

    typography 병합

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -6,22 +6,46 @@ import {useForm} from 'react-hook-form';
 import {SKILL_LIST} from '../constant';
 import {skillsListType} from '../types/skills';
 import Typography from '../components/atoms/typography';
+
+interface skillList extends skillsListType {
+  isSelect: boolean;
+}
+
 const Test = () => {
   const {register, handleSubmit} = useForm();
   const {
     colors: {
-      neutral: {LIGHT_GREY},
-      primary: {LIGHT_BLUE},
+      neutral: {LIGHT_GREY, WHITE},
+      primary: {LIGHT_BLUE, DARK_BLUE, MEDIUM_BLUE},
     },
   } = useTheme();
-  const [skillList, setSkillList] = React.useState<skillsListType[]>([]);
+  const [skillList, setSkillList] = React.useState<any[]>([]);
+
+  const [selectedList, setSelectedList] = React.useState<skillList[]>([]);
 
   const onChange = ({target: {value}}: React.ChangeEvent<HTMLInputElement>) => {
     if (value.trim() !== '') {
-      const match = SKILL_LIST?.filter(({name}) => name.includes(value));
+      const match = SKILL_LIST.map((skill) => {
+        const {name} = skill;
+        if (name.includes(value)) return {...skill, isSelect: false};
+        else return;
+      }).filter((skill) => skill);
+
       setSkillList(match);
     } else {
       setSkillList([]);
+    }
+  };
+
+  const onClickSkill = (value: string) => {
+    const isExists = selectedList.find(
+      ({value: selectedValue}) => value === selectedValue,
+    );
+    if (!isExists) {
+      const target = skillList.find(
+        ({value: skillListValue}: skillList) => value === skillListValue,
+      );
+      setSelectedList((prev) => [...prev, {...target, isSelect: true}]);
     }
   };
 
@@ -30,7 +54,7 @@ const Test = () => {
   };
 
   return (
-    <S.Container>
+    <SearchStyle.Wrapper>
       <SearchStyle.Container>
         <SearchStyle.Form
           onSubmit={onSubmit}
@@ -50,46 +74,79 @@ const Test = () => {
             <SearchIcon />
           </Button>
         </SearchStyle.Form>
+
         <SearchStyle.Tag>
-          {skillList?.map(({name, renderIcon}, index) => (
-            <SearchStyle.TagList
-              key={`${index}`}
-              padding={'12px 16px'}
-              borderRadius="24"
-              borderColor={LIGHT_GREY}>
-              <span>{renderIcon()}</span>
-              <Typography color={LIGHT_GREY}>{name}</Typography>
-            </SearchStyle.TagList>
-          ))}
+          <div style={{display: 'flex', gap: '5.33px'}}>
+            {skillList?.map(
+              ({name, value, renderIcon}: skillList, index: number) => (
+                <SearchStyle.TagList
+                  key={`${index}`}
+                  padding={'12px 16px'}
+                  borderRadius="24"
+                  borderColor={LIGHT_GREY}
+                  onClick={() => onClickSkill(value)}>
+                  <span>{renderIcon()}</span>
+                  <Typography weight="600" color={LIGHT_GREY}>
+                    {name}
+                  </Typography>
+                </SearchStyle.TagList>
+              ),
+            )}
+          </div>
         </SearchStyle.Tag>
+        {selectedList.length !== 0 && (
+          <React.Fragment>
+            <SearchStyle.Tag>
+              <div style={{display: 'flex', gap: '5.33px'}}>
+                {selectedList?.map(
+                  ({name, value, renderIcon}: skillList, index: number) => (
+                    <SearchStyle.TagList
+                      key={`${index}`}
+                      padding={'12px 16px'}
+                      borderRadius="24"
+                      borderColor={DARK_BLUE}>
+                      <span>{renderIcon()}</span>
+                      <Typography weight="600" color={DARK_BLUE}>
+                        {name}
+                      </Typography>
+                    </SearchStyle.TagList>
+                  ),
+                )}
+              </div>
+              <Button
+                padding="12px 16px"
+                borderRadius="8"
+                backgroundColor={MEDIUM_BLUE}>
+                <Typography weight="600" color={WHITE}>
+                  확인
+                </Typography>
+              </Button>
+            </SearchStyle.Tag>
+          </React.Fragment>
+        )}
       </SearchStyle.Container>
-    </S.Container>
+    </SearchStyle.Wrapper>
   );
 };
 
 export default Test;
 
-const S = {
-  Container: styled.div`
+const SearchStyle = {
+  Wrapper: styled.div`
     display: flex;
     flex: 1;
     flex-direction: column;
     justify-content: center;
     align-items: center;
   `,
-};
-const SearchStyle = {
   Container: styled.div`
     width: 100%;
+    padding: 24px;
     display: flex;
     flex-direction: column;
-    flex-wrap: wrap;
-    gap: 10px;
-
-    padding: 24px;
     border: 1px solid #abbed1;
     border-radius: 12px;
-    overflow-x: hidden;
+    gap: 16px;
   `,
   Form: styled.form<{backgroundColor: string; borderRadius: string}>`
     display: flex;
@@ -97,17 +154,20 @@ const SearchStyle = {
     background-color: ${({backgroundColor}) => backgroundColor};
     border-radius: ${({borderRadius}) => borderRadius + 'px'};
   `,
+
   Tag: styled.div`
+    overflow-x: auto;
     display: flex;
-    /* flex-wrap: wrap; */
+    justify-content: space-between;
     gap: 8px;
   `,
+
   TagList: styled(Button)`
     display: flex;
-    width: 112px;
-
+    /* width: 112px; */
     height: 40px;
     gap: 5.33px;
+
     align-items: center;
     svg {
       width: 17.3px;
