435b41a4b42d27e4d69f7780cd97a63262f0a7fb
BBAK_JUN <wnsguddl789@gmail.com>
2022-08-06 22:47:24 +0900

    dev : 오탈자 수정

--- src/envs/dev.ts
+++ src/envs/dev.ts
@@ -2,5 +2,5 @@ export const variables = {
   BASE_URL: 'http://localhost:1337',
   GITHUB_CLIENT_ID: '17c0d79be6d97bde6342',
   GITHUB_SECRET_KEY: 'fee1a961a337cbac00ac56abf70a1cce578092c9',
-  REDIRECT_URL: 'http://localhost:3000/callback',
+  REDIRECT_URL: 'http://localhost:3000/auth/callback',
 };
