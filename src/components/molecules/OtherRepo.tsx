4540a188231346694f2585b5f091c490e9cb19d7
BBAK_JUN <wnsguddl789@gmail.com>
2022-08-07 10:27:48 +0900

    dev : OtherRepo 변경

--- src/components/molecules/OtherRepo.tsx
+++ src/components/molecules/OtherRepo.tsx
@@ -1,20 +1,33 @@
 import React from 'react';
 import styled from 'styled-components';
-import {LikeIcon, NodeJsIcon} from '../atoms/Icon';
-import Typography from '../atoms/typography';
+import {LikeIcon, NodeJsIcon, Typography} from 'src/components/atoms';
+import {useTheme} from 'styled-components';
 
 interface OtherRepoProps {
-  data: {
-    title: string;
-    subTitle: string;
-  };
+  redirect: () => void;
+  title: string;
+  description: string;
+  used_language: string;
+  thumbnail?: string;
 }
 
-export const OtherRepo: React.FC<OtherRepoProps> = ({data: {title, subTitle}}) => {
+export const OtherRepo: React.FC<OtherRepoProps> = ({
+  redirect,
+  title,
+  description,
+  used_language,
+  thumbnail,
+}) => {
+  const [isClicked, setIsLiked] = React.useState<boolean>(false);
+  const {
+    colors: {
+      neutral: {DARK_GREY},
+    },
+  } = useTheme();
   return (
-    <RepoContainer>
-      <Thumbnail />
-      <Content>
+    <RepoStyle.Container>
+      <RepoStyle.Thumbnail src={thumbnail} onClick={redirect} />
+      <RepoStyle.Content>
         <Typography
           className="repoListTitle"
           size="22"
@@ -24,96 +37,85 @@ export const OtherRepo: React.FC<OtherRepoProps> = ({data: {title, subTitle}}) =
           {title}
         </Typography>
         <Typography
-          className="repoListSubTitle"
+          className="repoListDescription"
           size="16"
           weight="400"
           lineHeight="24"
-          color="#4D4D4D"
+          color={DARK_GREY}
           marginBottom={24}>
-          {subTitle}
+          {description}
         </Typography>
-        <Icons>
-          <IconWrapper>
-            <Icon>
-              <NodeJsIcon />
-            </Icon>
-            <Icon>
-              <NodeJsIcon />
-            </Icon>
-            <Icon>
-              <NodeJsIcon />
-            </Icon>
-            <Icon>
-              <NodeJsIcon />
-            </Icon>
-            <Icon>
-              <NodeJsIcon />
-            </Icon>
-          </IconWrapper>
-          <LikeIcon />
-        </Icons>
-      </Content>
-    </RepoContainer>
+        <RepoStyle.IconList>
+          <div style={{display: 'flex'}}>
+            {used_language.split('::')?.map((text: any, index: number) => (
+              <RepoStyle.Icon key={`used_language-${index}`}>
+                <NodeJsIcon />
+              </RepoStyle.Icon>
+            ))}
+          </div>
+          <div>{isClicked ? <LikeIcon /> : <LikeIcon />}</div>
+        </RepoStyle.IconList>
+      </RepoStyle.Content>
+    </RepoStyle.Container>
   );
 };
+const RepoStyle = {
+  Container: styled.div`
+    display: flex;
+    width: 100%;
+    height: 187px;
+    margin: 12px 0;
+    border-radius: 12px;
+    background: #ffffff;
+    box-shadow: 0px 2px 11px rgba(9, 33, 57, 0.1);
+    border-radius: 12px;
+  `,
+  Thumbnail: styled.img`
+    width: 40%;
+    max-width: 232px;
+    height: 100%;
+    border-top-left-radius: inherit;
+    border-bottom-left-radius: inherit;
+    background-color: #717171;
+    cursor: pointer;
+  `,
 
-const RepoContainer = styled.div`
-  display: flex;
-  width: 100%;
-  height: 187px;
-  margin: 12px 0;
-  border-radius: 12px;
-  background: #ffffff;
-  box-shadow: 0px 2px 11px rgba(9, 33, 57, 0.1);
-  border-radius: 12px;
-`;
-const Thumbnail = styled.img`
-  width: 40%;
-  max-width: 232px;
-  height: 100%;
-  border-top-left-radius: inherit;
-  border-bottom-left-radius: inherit;
-  background-color: #717171;
-`;
-const Content = styled.div`
-  width: 100%;
-  height: 100%;
-  padding: 24px;
-  overflow: hidden;
-  position: relative;
-
-  .repoListTitle {
+  Content: styled.div`
+    width: 100%;
+    height: 100%;
+    padding: 24px;
     overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-  }
-  .repoListSubTitle {
-    overflow: hidden;
-    text-overflow: ellipsis;
-    display: -webkit-box;
-    -webkit-line-clamp: 2;
-    -webkit-box-orient: vertical;
-  }
-`;
+    position: relative;
 
-const Icons = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-const IconWrapper = styled.div`
-  display: flex;
-`;
-const Icon = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 28px;
-  height: 28px;
-  margin-right: 0.5rem;
-  border-radius: 50%;
-  /* White */
-  background: #ffffff;
-  /* Sky */
-  border: 1px solid #dbedff;
-`;
+    .repoListTitle {
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
+    .repoListDescription {
+      overflow: hidden;
+      text-overflow: ellipsis;
+      display: -webkit-box;
+      -webkit-line-clamp: 2;
+      -webkit-box-orient: vertical;
+    }
+  `,
+  IconList: styled.ul`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  `,
+  Icon: styled.li`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 28px;
+    height: 28px;
+    margin-right: 0.5rem;
+    border-radius: 50%;
+    /* White */
+    background: #ffffff;
+    /* Sky */
+    border: 1px solid #dbedff;
+  `,
+};
